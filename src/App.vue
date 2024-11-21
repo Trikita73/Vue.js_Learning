105ff26ce27ab6bab25ea30c68882160c2b48372
Trikita73 <diachenkoandrii7@gmail.com>
2024-11-22 01:42:29 +0200

    app_vue

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,43 @@
-<script>
-
-</script>
-
-<template>
-  <h1>Hello</h1>
-</template>
-
+// css
 <style scoped>
-h1 {
-  color: red;
+h3 {
+  font-weight: lighter;
+}
+
+p {
+  color: rgb(94, 42, 42);
 }
 </style>
+
+// Java Script
+<script>
+export default {
+  data() {
+    return {
+      info: 'Title!',
+      someInfo: 'Anons of message',
+      someClick: 'text of click'
+    }
+  },
+  methods: {
+    userData () {
+      this.info = 'Some new'
+    },
+    userClick (text = 'New text') {
+      this.someClick = text
+    }
+  }
+}
+</script>
+
+// HTML
+<template>
+  <h3>{{ info }}</h3>
+  <p>{{ someInfo }}</p>
+  <p>{{ someClick }}</p>
+
+  <button type="button" v-on:click="userData ()">Отправить</button>
+  <button type="button" @mouseenter="this.someInfo = 'something interesting'">Наведи</button>
+  <button type="button" v-on:click="userClick ('New text')">Кликни2</button>
+</template>
+
