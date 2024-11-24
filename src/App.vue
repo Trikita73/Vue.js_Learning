eafb07cd0c2c5a94c2c12cbec3289f18a688802f
Trikita73 <diachenkoandrii7@gmail.com>
2024-11-25 00:51:25 +0200

    work_with_users

--- src/App.vue
+++ src/App.vue
@@ -59,14 +59,38 @@ export default {
 export default {
   data() {
     return {
+      users: [],
+      error: '',
       userName: '',
       userPass: '',
       userEmail: ''
     }
   },
   methods: {
+    /*
     insertData(val) {
       this.userName = val
+    }
+    */
+    sendData() {
+      if(this.userName == '') {
+        this.error = 'Вы не ввели имя!';
+        return;
+      } else if (this.userPass == '') {
+        this.error = 'Вы не ввели пароль!';
+        return;
+      } else if (this.userEmail == '') {
+        this.error = 'Вы не ввели email!';
+        retun;
+      }
+
+      this.error = '';
+
+      this.users.push({
+        name: this.userName,
+        pass: this.userPass,
+        email: this.userEmail
+      })
     }
   }
 }
@@ -74,8 +98,31 @@ export default {
 
 // HTML
 <template>
-  <input type="text" @input="insertData($event.target.value)" placeholder="Имя">
-  <input type="password" placeholder="Пароль">
-  <input type="email" placeholder="Email">
+  <!-- <input type="text" @input="insertData($event.target.value)" placeholder="Имя"> -->
+  <input type="text" v-model="userName" placeholder="Имя">
+  <input type="password" v-model="userPass" placeholder="Пароль">
+  <input type="email" v-model="userEmail" placeholder="Email">
+  <!--
   <p> {{ userName }}</p>
+  <p> {{ userPass }}</p>
+  <p> {{ userEmail }}</p>
+  -->
+  <p className="error">{{ error }}</p>
+  <button @click="sendData()">Отправить</button>
+  <!--<p>{{ users }}</p>-->
+
+  <div v-if="users.length == 0">
+    You don't have users
+  </div>
+  <div v-else-if="users.length == 1">
+    You have 1 element
+  </div>
+  <div v-else>
+    You have more than 1 element
+  </div>
+
+  <div v-for="(el, index) in users" :key="index">
+    <h3>{{ el.name }}</h3>
+    <p>{{ el.email }} - <b>{{ el.pass }}</b></p>
+  </div>
 </template>
