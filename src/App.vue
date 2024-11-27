5280ac7d0c157e2c6d57a1fb40e08c6f9362bdd4
Trikita73 <diachenkoandrii7@gmail.com>
2024-11-27 23:15:54 +0200

    api_weather

--- src/App.vue
+++ src/App.vue
@@ -249,21 +249,36 @@ export default {
     <h1>Погодное приложение </h1>
     <p>Узнать погоду в {{ city  == "" ? "вашем городе": cityName }}</p>
     <input type="text" v-model="city" placeholder="Введите город">
-    <button v-if="city != ''">Получить результат</button>
+    <button v-if="city != ''" @click="getWeather()">Получить результат</button>
     <button disabled v-else>Введите название города</button>
+    <p class="error">{{ error }}</p>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   data() {
     return {
-      city: ""
+      city: "",
+      error: ""
     }
   },
   computed: {
     cityName() {
       return "<<" + this.city + ">"
+    }
+  },
+  methods: {
+    getWeather() {
+      if(this.city.trim().length < 2) {
+        this.error = "Нужно название более одного символа!"
+        return false
+      }
+      this.error = ""
+
+      axios.get('')
     }
   }
 }
@@ -321,5 +336,9 @@ export default {
 
   .wrapper button:hover {
     transform: scale(1.1) translate(-5px);
+  }
+  
+  .error {
+    color: #d03933;
   }
 </style>
