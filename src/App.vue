a36e51fc6bc12209482f78b4a146b5570ee32b5f
Trikita73 <diachenkoandrii7@gmail.com>
2024-11-27 02:35:15 +0200

    api_conditions

--- src/App.vue
+++ src/App.vue
@@ -246,13 +246,27 @@ export default {
 
 <template>
   <div class="wrapper">
-    <h1>Погодное приложение</h1>
-    <p>Узнать погоду в вашем городе</p>
+    <h1>Погодное приложение </h1>
+    <p>Узнать погоду в {{ city  == "" ? "вашем городе": cityName }}</p>
+    <input type="text" v-model="city" placeholder="Введите город">
+    <button v-if="city != ''">Получить результат</button>
+    <button disabled v-else>Введите название города</button>
   </div>
 </template>
 
 <script>
-
+export default {
+  data() {
+    return {
+      city: ""
+    }
+  },
+  computed: {
+    cityName() {
+      return "<<" + this.city + ">"
+    }
+  }
+}
 </script>
 
 <style scoped>
@@ -272,5 +286,40 @@ export default {
 
   .wrapper p {
     margin-top: 20px;
+  }
+
+  .wrapper input {
+    margin-top: 30px;
+    background: transparent;
+    border: 0;
+    border-bottom: 2px solid #110813;
+    color: #fcfcfc;
+    font-size: 14px;
+    padding: 5px 8 px;
+    outline: none;
+  }
+
+  .wrapper input:focus {
+    border-bottom-color: #6e2d7d;
+  }
+
+  .wrapper button:disabled {
+    background: #7e6a2e;
+    cursor: not-allowed;
+  }
+
+  .wrapper button {
+    background: #e3bc4b;
+    color: #fff;
+    border-radius: 10px;
+    border: 2px solid #b99935;
+    padding: 10px 15px;
+    margin-left: 20px;
+    cursor: pointer;
+    transition: transform 500ms ease;
+  }
+
+  .wrapper button:hover {
+    transform: scale(1.1) translate(-5px);
   }
 </style>
